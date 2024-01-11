00aa2bc1bbb61e73eee1f933d200d9cee0721d5e
clay-wangzhi <clay.wangzhi@gmail.com>
2024-01-11 16:47:08 +0800

    perf: The sharedIndexInformer has started.

--- pkg/shared/k8s_shared_informer.go
+++ pkg/shared/k8s_shared_informer.go
@@ -115,8 +115,6 @@ func (c *Controller) Run(workers int, stopCh chan struct{}) {
 	defer c.queue.ShutDown()
 	klog.Info("Starting Pod controller")
 
-	go c.informer.Run(stopCh)
-
 	// Wait for all involved caches to be synced, before processing items from the queue is started
 	if !cache.WaitForCacheSync(stopCh, c.informer.HasSynced) {
 		runtime.HandleError(fmt.Errorf("Timed out waiting for caches to sync"))
